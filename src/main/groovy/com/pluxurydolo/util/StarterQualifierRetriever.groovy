66158017272c796b4c3a392633b5a47710c604ec
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-05-17 23:29:36 +0300

    17.05.2026

--- src/main/groovy/com/pluxurydolo/util/StarterQualifierRetriever.groovy
+++ src/main/groovy/com/pluxurydolo/util/StarterQualifierRetriever.groovy
@@ -2,15 +2,23 @@ package com.pluxurydolo.util
 
 import org.gradle.api.logging.Logger
 
+import java.util.stream.Collectors
+
+import static java.util.Arrays.stream
+
 class StarterQualifierRetriever {
     static String retrieve(String projectName, Logger logger) {
         String[] qualifierParts = projectName.split('-')
-        String firstPart = qualifierParts[0]
-        String lastPart = qualifierParts[qualifierParts.size() - 1]
-        String starterQualifier = "$firstPart-$lastPart"
+        String starterQualifier = starterQualifier(qualifierParts)
 
         logger.lifecycle("lugw [logs-plugin] Название стартера: $starterQualifier")
 
         return starterQualifier
+    }
+
+    private static String starterQualifier(String[] qualifierParts) {
+        return stream(qualifierParts)
+                .filter { part -> part != 'spring' && part != 'boot' }
+                .collect(Collectors.joining('-'))
     }
 }
